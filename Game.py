b4e58a7f5bec88698f902c5275cf9b324e153e1e
tbarrin4 <tbarrin4@students.kennesaw.edu>
2020-11-12 11:34:13 -0500

    Create Game.py
    Updated the intro to the game. Provided users with instructions to play.

---
+++ Game.py
@@ -0,0 +1,4 @@
+#remember to save and test your code often
+print("\n THAD'S SCI-FI MYSTERY GAME!")
+print('\n Once you start playing, you will be asked questions throughout the game. You will have two options for each question, and you must enter "1" or "2" to select your answer.')
+print('\n Choosing "1" will select the first option, and choosing "2" will select the second option.')
