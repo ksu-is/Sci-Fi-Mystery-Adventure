85b1902a6f53bd5ea886c342da5d82adeac30d37
tbarrin4 <tbarrin4@students.kennesaw.edu>
2020-11-19 09:31:02 -0500

    Updated code and roadmap.
    Began the beginning stages of the code, as well as updating the checkbox in project roadmap.

--- Game.py
+++ Game.py
@@ -1,4 +1,17 @@
 #remember to save and test your code often
 print("\n THAD'S SCI-FI MYSTERY GAME!")
 print('\n Once you start playing, you will be asked questions throughout the game. You will have two options for each question, and you must enter "1" or "2" to select your answer.')
-print('\n Choosing "1" will select the first option, and choosing "2" will select the second option.')
+print('\n Typing "1" will select the first option, and typing "2" will select the second option.')
+
+print('\n .........')
+print()
+
+print('You awake in an unfamiliar place. After glancing around, you determine that you are most likely in the cargo deck of a spaceship.')
+first_q = input('Option 1: Leave the cargo deck through the main door. Option 2: Leave the cargo deck by climbing the ladder to the hatch. ')
+while True:
+    if first_q == '1':
+        print('You open the main door and enter a dimly lit hallway. The ground is wet.')
+    elif first_q == '2':
+        print('You climb the ladder and go through the hatch. You appear to be in the living quarters.')
+    else:
+        print('Invalid response. Try again.')
