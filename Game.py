7d05153530d12c1967a5c9efbdb4090887802395
tbarrin4 <69830814+tbarrin4@users.noreply.github.com>
2020-11-20 16:38:50 -0500

    Updated Game code with more levels in the game.
    I have begun writing more of the levels in the game. I also compared my code with the forked repository, but I decided to find other codes that work a little bit better. Here is the URL for a code I am referencing for the while True loops: https://trinket.io/python/e5a03e7cbc

--- Game.py
+++ Game.py
@@ -12,8 +12,22 @@ print('You awake in an unfamiliar place. After glancing around, you determine th
 first_q = input('Option 1: Leave the cargo deck through the main door. Option 2: Leave the cargo deck by climbing the ladder to the hatch. ')
 while True:
     if first_q == '1':
-        print('You open the main door and enter a dimly lit hallway. The ground is wet.')
+        print('\n You open the main door and enter a dimly lit hallway. There is a being in the shadows that threatens your safety.')
+        door_q = input('Option 1: Attempt to fight the creature. Option 2: Run away towards the stairs.')
+        while True:
+            if door_q == '1':
+                print('\n You try to fight, but the creature unfortunetly eats you. END OF GAME.')
+                break
+            elif door_q == '2':
+                print('\n You manage to escape from the creature. You have reached another hallway with two doors.')
     elif first_q == '2':
-        print('You climb the ladder and go through the hatch. You appear to be in the living quarters.')
+        print('\n You climb the ladder and go through the hatch. You appear to be in the living quarters.')
+        hatch_q = input('Option 1: You explore the living quarters in hope of finding any information. Option 2: Feeling tired, you decide to take a nap on one of the beds.')
+        while True:
+            if hatch_q == '1':
+                print('\n After looking around, you find a hologram message near one of the beds. The hologram shows a shipmate explaining that an unknown pirate spacecraft has docked onto the ship you are currently in. The hologram cuts out suddenly.')
+            elif hatch_q == '2':
+                print('\n While sleeping, you are eaten, END OF GAME.')
+                break
     else:
-        print('Invalid response. Try again.')
+        print('\n Invalid response. Try again.')
