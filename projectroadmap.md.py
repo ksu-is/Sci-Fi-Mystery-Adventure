62e7b2f1032191657ed9a3328ad7090f039aa79b
tbarrin4 <69830814+tbarrin4@users.noreply.github.com>
2020-11-20 17:36:55 -0500

    Updated projectroadmap.md
    I have checked all of the boxes for the second sprint, and have completed 6 code commits.

---
+++ projectroadmap.md.py
@@ -0,0 +1,21 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> First sprint:
+[x] Fork repository to KSU-IS
+[x] Pick out LICENSE for code
+[x] Update README.md
+[x] Create projectroadmap.md
+[x] Upadte projectroadmap.md
+[x] Evaluate repository that is similar to what I will be making.
+
+Second sprint:
+[x] Begin writing code for game
+[x] Compare code with forked repository
+[x] Commit at least 6 code changes
+[x] Paste 2 URLs to code commits
+
+
+Third sprint:
+[ ] Continue writing code and testing
+[ ] Create one PowerPoint slide
+[ ] Upload slide
